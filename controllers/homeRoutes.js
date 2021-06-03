001caa4105a6c7256996ad2991081cba09b05afd
Hayden Jones <haychjones@gmail.com>
2021-06-03 13:28:07 +1000

    login styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,13 @@ router.get("/", (req, res) => {
   res.render("layouts/main");
 });
 
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  } else {
+    res.render("login");
+  }
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-const login = require("./login");
 
 router.use("/", homeRoutes);
-
-// router.use("/login", login);
 
 // router.use("/api", apiRoutes);
 
